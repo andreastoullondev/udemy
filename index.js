eef5d2bba53c26c7bc9e187643f369ab44b9e92f
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-07 23:18:37 -0300

    metodo with vou passar todos os metos para o gitgnor

--- index.js
+++ index.js
@@ -32,5 +32,7 @@ const array3 = [6, 7, 9];
 //const resultado = array1.concat(array2, array3);
 //array1.unshift(array2, array3) um dado no começo da array nesse casso adicionou a string array2 e 3
 //array1.shift(3) reomove o primeiro dado da array ,não consegui remover outro 
+// metodo with me permiti acessar um escopo de um objeto pelo meu objeto atual(gostei e estou pensando como vou usar)
+
 console.log(array1);
 
