09b50f9c951d62651da025f9fede6bdc6c524f40
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-07 23:15:40 -0300

    adicionando metodos que futuramente posso usar em projetos

--- index.js
+++ index.js
@@ -23,3 +23,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
     atualizaNumeroNoHTML()
 });
+// concat quando ele é usado ele retorno uma array com tudo dentro sem modificar as originais;
+
+const array1 = [5, 2, 3];
+const array2 = [8, 1, 4];
+const array3 = [6, 7, 9];
+
+//const resultado = array1.concat(array2, array3);
+//array1.unshift(array2, array3) um dado no começo da array nesse casso adicionou a string array2 e 3
+//array1.shift(3) reomove o primeiro dado da array ,não consegui remover outro 
+console.log(array1);
+
