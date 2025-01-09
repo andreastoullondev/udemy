12bf05d8a1729becd10b27bbf97fa837da9b1c53
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-08 22:35:33 -0300

    adicionado for

--- projetos/fibonace.js
+++ projetos/fibonace.js
@@ -1,9 +1,18 @@
-let penultimo = 0
+let penultimo = 0;
 
-let ultimo = 1 
+let ultimo = 1 ;
 
-let proporcao = 0
+let proporcao = 0;
 
+const ciclosDesejados = 20;
 
+for(let contador = 0;contador <=ciclosDesejados;contador++){
+    let proximo = penultimo + ultimo;
+    penultimo = ultimo;
+    ultimo = proximo;
+
+    console;log(proximo);
+}
 
 const fibonnaci = [];
+
