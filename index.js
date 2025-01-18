f6bc6af0c2f60adedb66a6ceca6d5eaa432c628a
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-18 13:42:47 -0300

    Fibonacci

--- index.js
+++ index.js
@@ -41,3 +41,28 @@ const array3 = [6, 7, 9];
 //const resultado = array1.join(', '); usado para unir elementos de uma array em uma unica string 
 console.log(resultado);
 
+//Fibonacci
+let penultimo = 0;
+
+let ultimo = 1 ;
+
+let proporcao = 0;
+
+const ciclosDesejados = 20;
+
+for(let contador = 0;contador <=ciclosDesejados;contador++){
+    let proximo = penultimo + ultimo
+    penultimo = ultimo
+    ultimo = proximo
+
+    console.log(proximo)
+
+    if(contador > 0){
+        proporcao = ultimo / penultimo
+    }
+
+}
+console.log(proporcao);
+
+
+
