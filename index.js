1f8ba61bac4fba44f0a4ab920d7ac2732ab651df
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-19 17:29:36 -0300

    janela modal pronta

--- index.js
+++ index.js
@@ -61,7 +61,13 @@ lamp.addEventListener('dblclick', lampBroken);
 
 function abriModal(){
     const modal = document.getElementById('janela-modal');
-    modal.classList.add('abrir')
+    modal.classList.add('abrir');
+
+    modal.addEventListener('click', (e) =>{
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+            modal.classList.remove('abrir')
+        }
+    })
 }
 
 
