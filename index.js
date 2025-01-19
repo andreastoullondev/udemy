6da8911939eb45a8a86e2022e3e5f0746c3fc2a2
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-18 22:29:00 -0300

    lampada off

--- index.js
+++ index.js
@@ -36,12 +36,16 @@ const lamp = document.getElementById('lamp');
 function lampOn(){
     lamp.src = '/img/ligada.jpg';
 }
+function lampOff(){
+    lamp.src = '/img/desligada.jpg';
+}
 
 
 
 
 
 turnOn.addEventListener( 'click', lampOn);
+turnOff.addEventListener( 'click', lampOff);
 
 
 
