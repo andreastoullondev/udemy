5d7fe8e95155ff4415ce48e7cee4848e34e3f26d
Andreas Toullon <andreasgitinit@gmail.com>
2025-01-18 22:25:19 -0300

    função lamp on

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
 
+function lampOn(){
+    lamp.src = '/img/ligada.jpg';
+}
+
+
+
+
+
+turnOn.addEventListener( 'click', lampOn);
+
 
 
 
